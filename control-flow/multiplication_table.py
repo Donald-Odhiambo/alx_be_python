0dd57acffa58a2162dbd45d0a91e2bf97fad04ba
Oketch Donald  Odhiambo <primedohnix@yahoo.com>
2025-09-18 13:14:43 +0300

    modify multiplication_table file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,8 +1,10 @@
 #Multiplication Table Generator
 
-number = input("Enter a number to see its multiplication table:.")
+number = input("Enter a number to see its multiplication table:")
 
 for num in range(1 ,int(number) + 1):
+
     product = int(number) * num
+
     print(number , '*', num, '=', product)
     
