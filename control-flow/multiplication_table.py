16f1ade246f209c8d421094befd8342189f147a9
Oketch Donald  Odhiambo <primedohnix@yahoo.com>
2025-09-19 12:32:07 +0300

    Modify Multiplication table codes

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,10 +1,7 @@
-#Multiplication Table Generator
+#Multiplication Table Generator using for loop
 
-number = input("Enter a number to see its multiplication table:")
+number = int(input("Enter a number to see its multiplication table:"))
 
-for num in range(1 ,int(number) + 1):
-
-    product = int(number) * num
-
-    print(number , '*', num, '=', product)
-    
+for num in range(1,11):
+    result = number * num
+    print(f"{number} * {num} =",result)
