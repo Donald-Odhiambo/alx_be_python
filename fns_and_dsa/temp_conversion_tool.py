3d96ffaef9596dd176ad45933207f4a90940a32e
Oketch Donald  Odhiambo <primedohnix@yahoo.com>
2025-09-28 09:19:36 +0300

    Modify temperature conversion file

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,19 +1,17 @@
-# temp_conversion_tool.py
 
-# Global conversion factors
+
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
-# Conversion functions
+
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius using the global factor."""
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit using the global factor."""
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) 
 
-# User interaction
 def main():
     try:
         temp_input = input("Enter the temperature to convert: ")
